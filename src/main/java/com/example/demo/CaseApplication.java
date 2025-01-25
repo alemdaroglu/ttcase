85d445127d31cd32bc62f2ec6f8eb9d1c453dbf9
Arda Alemdaroglu <arda.alemdaroglu@gmail.com>
2025-01-25 17:35:10 +0300

    add swagger support

--- src/main/java/com/example/demo/CaseApplication.java
+++ src/main/java/com/example/demo/CaseApplication.java
@@ -11,11 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan(basePackages = "com.example")
 public class CaseApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(CaseApplication.class, args);
 	}
